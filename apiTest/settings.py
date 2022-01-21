e9f5c548ea5891a1ab4523bbed5c804e7a0b2a7c
jay <jimniz01@gmail.com>
2022-01-21 14:48:18 -0500

    Settings Edited

--- apiTest/settings.py
+++ apiTest/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'y=7&b1f&4w2k-_0oobbg$3l84fqgjdldrbiita_s)d3onf(h3m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
